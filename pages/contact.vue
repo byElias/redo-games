6beb1a0b4de4447193e9cc1e1aad781d5c1dd302
reads <s.read@walz.de>
2025-01-24 12:15:11 +0100

    hihi haha coole mail formular ya saleme

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,31 +5,41 @@
         <h1 class="text-5xl">contact</h1>
         <p class="pt-16">E-Mail Adresse</p>
         <a href="mailto:redo.games.message@gmail.com" class="pt-4">redo.games.message@gmail.com</a>
-            
+
         <div class="flex flex-col pt-10 xl:flex-row xl:space-x-10">
-          <SysRedContactField :title="'Vorname'" :width="'100%'"  :heigth="'35px'"/>
-          <SysRedContactField :title="'Nachname'" :width="'100%'" :heigth="'35px'"/>
-          <SysRedContactField :title="'Email *'" :width="'100%'" :heigth="'35px'"/>
+          <SysRedContactField v-model="firstname" :title="'Vorname'" :width="'100%'" :heigth="'35px'" />
+          <SysRedContactField v-model="lastname" :title="'Nachname'" :width="'100%'" :heigth="'35px'" />
+          <SysRedContactField v-model="mailAdress" :title="'Email *'" :width="'100%'" :heigth="'35px'" />
         </div>
 
         <div class="my-10">
-          <SysRedContactField :title="'Nachricht'" :width="'100%'" :heigth="'70px'"/> 
+          <SysRedContactField v-model="textMessage" :title="'Nachricht'" :width="'100%'" :heigth="'70px'" />
         </div>
 
-        <button class="w-[35%] h-[35px] bg-red-700 rounded-lg hover:bg-red-900 mb-10">Senden</button>
-            
+        <button @click="SendIt" class="w-[35%] h-[35px] bg-red-700 rounded-lg hover:bg-red-900 mb-10">Senden</button>
+
       </div>
-    </div>        
+    </div>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import SysRedContactField from '~/components/SysRedContactField.vue';
 
-export default {
-  components: {
-    SysRedContactField,
-  },
-};
-</script>
+const SendIt = async () => {
+  await $fetch('/api/contactFormularAPI', {
+    body: {
+      firstname: firstname.value,
+      lastname: lastname.value,
+      mailAdress: mailAdress.value,
+      textMessage: textMessage.value
+    },
+    method: 'POST'
+  })
+}
 
+const firstname = ref('')
+const lastname = ref('')
+const mailAdress = ref('')
+const textMessage = ref('') 
+</script>
